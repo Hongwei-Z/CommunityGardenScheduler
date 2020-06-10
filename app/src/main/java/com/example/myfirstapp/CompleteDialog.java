487e0f63d682605f1238f16097ac0d8a5c3cd815
jbai <jn528470@dal.ca>
2020-06-10 13:04:20 -0300

    complete dialog

---
+++ app/src/main/java/com/example/myfirstapp/CompleteDialog.java
@@ -0,0 +1,23 @@
+package com.example.myfirstapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.os.Bundle;
+import android.app.Dialog;
+
+public class CompleteDialog extends AppCompatDialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle saved){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("TASK COMPLETED").setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        return builder.create();
+
+    }
+}
