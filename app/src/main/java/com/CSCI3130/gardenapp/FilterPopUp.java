5db42e956550398133bc18f7ffb7b2395cbf54b1
hongwei <hn507820@dal.ca>
2020-06-11 14:17:06 -0300

    Add Filter files

---
+++ app/src/main/java/com/CSCI3130/gardenapp/FilterPopUp.java
@@ -0,0 +1,14 @@
+package com.CSCI3130.gardenapp;
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * This class is the filter popup window.
+ * @author Hongwei Zhang (B00780843)
+ */
+class FilterPopUp extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filter);
+    }
+}
