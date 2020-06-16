2db94900f8cf70e65d0f15e6ec27a0c364b84bde
hongwei <hn507820@dal.ca>
2020-06-16 12:16:38 -0300

    Add a hardcoding method(afterFilter) to display task-list after the filter, but for an unknown reason, it can't display.

--- app/src/main/java/com/CSCI3130/gardenapp/FilterActivity.java
+++ app/src/main/java/com/CSCI3130/gardenapp/FilterActivity.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -51,10 +56,12 @@ public class FilterActivity extends AppCompatActivity {
          * @Button applyButton is the "Apply" button on filter window
          */
         final Button applyButton = myDialog.findViewById(R.id.applyButton);
+        final TextView result = myDialog.findViewById(R.id.textView0);
         applyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 myDialog.dismiss();
+                result.setText(afterFilter());
             }
         });
 
@@ -92,5 +99,46 @@ public class FilterActivity extends AppCompatActivity {
                 dateBetween.add(year*10000+(month+1)*100+day);
             }
         },year,month+1,day).show();
+    }
+
+    public String afterFilter(){
+        String result="";
+
+        //ArrayList<Task> taskList copied from TaskViewList.class
+        ArrayList<Task> taskList = new ArrayList<>();
+        taskList.add(new Task("Cover tomatoes", "cover if raining", 4, "", "June 4th, 2020"));
+        taskList.add(new Task("Water all of the carrots", "water them a lot", 2, "Bob", "June 2nd, 2020"));
+        taskList.add(new Task("Plant cucumber transplants", "", 1, "Bill", "June 10th, 2020"));
+        taskList.add(new Task("Dig some dirt", "pile it", 3,  "", "June 20th, 2020"));
+        taskList.add(new Task("Water more carrots", "water them a lot", 5, "", "June 30th, 2020"));
+        taskList.add(new Task("Plant tomato transplants", "", 1, "Bill", "June 10th, 2020"));
+        taskList.add(new Task("Dig some more dirt", "", 3, "Bill", "June 4th, 2020"));
+        taskList.add(new Task("Water cucumber", "water them a lot", 3, "", "June 16th, 2020"));
+        taskList.add(new Task("Add compost", "", 1, "Bill", "June 10th, 2020"));
+
+        ArrayList<Task> taskListAfter = new ArrayList<>();
+        String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        for (int i=0; i<taskList.size(); i++){
+            String date = taskList.get(i).getDate();
+            String[] spiltDate = date.split("\\s");
+            String m = spiltDate[0];
+            String d = spiltDate[1];
+            String y = spiltDate[2];
+            int finalDate = 0;
+            if (d.contains("th,")){
+                d.replace("th,", "");
+            }
+            for (int j=0; j<month.length; j++){
+                if (month[j].equals(m)){
+                    finalDate = j*100;
+                }
+            }
+            finalDate+=Integer.parseInt(y)*10000;
+            finalDate+=Integer.parseInt(d);
+            if (finalDate <= dateBetween.get(1) && finalDate >= dateBetween.get(0)){
+                result+=taskList.get(i).getDescription()+"\n";
+            }
+        }
+        return result;
     }
 }
