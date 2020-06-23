8672a646e5bd667dfed81499e644f8232c8022a2
hongwei <hn507820@dal.ca>
2020-06-23 15:25:32 -0300

    Create an independent method for each button, all the methods in onCreate have been removed as suggested by Liam. All buttons work normally.

--- app/src/main/java/com/CSCI3130/gardenapp/FilterPopUp.java
+++ app/src/main/java/com/CSCI3130/gardenapp/FilterPopUp.java
@@ -13,94 +13,77 @@ import java.util.Calendar;
  * @author Hongwei Zhang, Liam Hebert, Elizabeth Eddy
  */
 public class FilterPopUp extends Activity {
-    //ArrayList dateBetween used to save two dates which user selected.
+    //ArrayList dateBetween used to save two dates which user selected
     ArrayList<Integer> dateBetween;
     int priority = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         dateBetween = new ArrayList<>(2);
+
+        //to avoid null pointer exception
+        dateBetween.add(1);
+        dateBetween.add(2);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filter);
-
-        /**
-         * @Button selectStartDate is the "Start Date" button on filter window
-         * @Button selectEndDate is the "End Date" button on filter window
-         */
-        final Button selectStartDate = findViewById(R.id.startDateButton);
-        final Button selectEndDate = findViewById(R.id.endDateButton);
-        //TODO - Ditto from the below
-        selectStartDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectCalendar(selectStartDate);
-            }
-        });
-        selectEndDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectCalendar(selectEndDate);
-            }
-        });
-
-        /***
-         * @Button applyButton is the "Apply" button on filter window
-         */
-        final Button applyButton = findViewById(R.id.applyButton);
-        //TODO - Check for null values. This should also be in an OnClick event function, not in the create function
-        applyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                applyButton.setText(dateBetween.get(0)+", "+dateBetween.get(1)+", "+priority);
-            }
-        });
-
-        //Priority buttons
-        final Button p1 = (Button) findViewById(R.id.priority1Button);
-        final Button p2 = (Button) findViewById(R.id.priority2Button);
-        final Button p3 = (Button) findViewById(R.id.priority3Button);
-        final Button p4 = (Button) findViewById(R.id.priority4Button);
-        final Button p5 = (Button) findViewById(R.id.priority5Button);
-
-        /***
-         * @Button clearButton is the "Clear" button on filter window
-         */
-        Button clearButton = findViewById(R.id.clearButton);
-        //TODO - Ditto from the top
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                selectStartDate.setText("Start Date");
-                selectEndDate.setText("End Date");
-                applyButton.setText("APPLY");
-                dateBetween.clear();
-                p1.setScaleX(1);
-                p1.setScaleY(1);
-                p2.setScaleX(1);
-                p2.setScaleY(1);
-                p3.setScaleX(1);
-                p3.setScaleY(1);
-                p4.setScaleX(1);
-                p4.setScaleY(1);
-                p5.setScaleX(1);
-                p5.setScaleY(1);
-            }
-        });
     }
 
-    /**
-     * selectCalendar method, open the date picker
-     * @param b is a select date button
+    //clearMethod, click the Clear button, perform the clear function
+    public void clearMethod(View view){
+        Button startDate = findViewById(R.id.startDateButton);
+        Button endDate = findViewById(R.id.endDateButton);
+        Button apply = findViewById(R.id.applyButton);
+        Button p1 = findViewById(R.id.priority1Button);
+        Button p2 = findViewById(R.id.priority2Button);
+        Button p3 = findViewById(R.id.priority3Button);
+        Button p4 = findViewById(R.id.priority4Button);
+        Button p5 = findViewById(R.id.priority5Button);
+        startDate.setText("Start Date");
+        endDate.setText("End Date");
+        apply.setText("APPLY");
+        dateBetween.clear();
+        p1.setScaleX(1);
+        p1.setScaleY(1);
+        p2.setScaleX(1);
+        p2.setScaleY(1);
+        p3.setScaleX(1);
+        p3.setScaleY(1);
+        p4.setScaleX(1);
+        p4.setScaleY(1);
+        p5.setScaleX(1);
+        p5.setScaleY(1);
+    }
+
+    /***
+     * applyMethod, click the APPLY button, save selected date and priority
+     * Haven't connected to the database,
+     * the button is temporarily displayed strings to indicate that the button is functional
      */
-    public void selectCalendar(final Button b){
+    public void applyMethod(View view){
+        Button apply = findViewById(R.id.applyButton);
+        apply.setText(dateBetween.get(0)+", "+dateBetween.get(1)+", "+priority);
+    }
+
+    //selectCalendar, used to call the DatePicker, select dates
+    public void selectCalendar(final View view){
+        final Button startDate = findViewById(R.id.startDateButton);
+        final Button endDate = findViewById(R.id.endDateButton);
         Calendar date = Calendar.getInstance();
         int year = date.get(Calendar.YEAR);
         int month = date.get(Calendar.MONTH);
         int day = date.get(Calendar.DAY_OF_MONTH);
         new DatePickerDialog(FilterPopUp.this, new DatePickerDialog.OnDateSetListener() {
-            @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                b.setText(year+", "+(month+1)+", "+day);
-                dateBetween.add(year*10000+(month+1)*100+day);
+                switch (view.getId()){
+                    case R.id.startDateButton:
+                        startDate.setText(year+", "+(month+1)+", "+day);
+                        dateBetween.set(0, year*10000+(month+1)*100+day);
+                        break;
+                    case R.id.endDateButton:
+                        endDate.setText(year+", "+(month+1)+", "+day);
+                        dateBetween.set(1, year*10000+(month+1)*100+day);
+                        break;
+                }
             }
         },year,month+1,day).show();
     }
@@ -136,11 +119,11 @@ public class FilterPopUp extends Activity {
      * @param priority is the priority number
      */
     public void highlightButton(int priority){
-        Button p1 = (Button) findViewById(R.id.priority1Button);
-        Button p2 = (Button) findViewById(R.id.priority2Button);
-        Button p3 = (Button) findViewById(R.id.priority3Button);
-        Button p4 = (Button) findViewById(R.id.priority4Button);
-        Button p5 = (Button) findViewById(R.id.priority5Button);
+        Button p1 = findViewById(R.id.priority1Button);
+        Button p2 = findViewById(R.id.priority2Button);
+        Button p3 = findViewById(R.id.priority3Button);
+        Button p4 = findViewById(R.id.priority4Button);
+        Button p5 = findViewById(R.id.priority5Button);
         switch (priority){
             case 1:
                 p1.setScaleX((float)1.2);
