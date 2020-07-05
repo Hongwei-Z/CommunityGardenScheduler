b963de474686e4fc143efda9f68ebfc75f89dafe
Logan Sutherland <sutherland@dal.ca>
2020-07-05 12:59:28 -0300

    Added further functionality and register button

--- app/src/main/java/com/CSCI3130/gardenapp/TaskDetailInfo.java
+++ app/src/main/java/com/CSCI3130/gardenapp/TaskDetailInfo.java
@@ -3,7 +3,6 @@ package com.CSCI3130.gardenapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,37 +10,24 @@ import android.widget.Toast;
 
 import com.CSCI3130.gardenapp.util.DateFormatUtils;
 import com.CSCI3130.gardenapp.util.data.Task;
-import com.CSCI3130.gardenapp.util.data.User;
-
-import org.w3c.dom.Text;
-
-import java.text.DateFormat;
 
 
 public class TaskDetailInfo extends AppCompatActivity {
 
     private Button completeButton;
-    private Button backlist;
-    private TextView textPriority;
     private TextView description;
     private TextView dueDate;
     private TextView taskTitle;
     private TextView location;
     private TextView repeatCon;
+    private TextView priority;
+    private TextView priorityColor;
     private Task task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail_info);
-
-        backlist = (Button)findViewById(R.id.backtolistbutton);
-        backlist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                backtotasklist();
-            }
-        });
 
         completeButton = (Button)findViewById(R.id.buttonComplete);
         completeButton.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +42,8 @@ public class TaskDetailInfo extends AppCompatActivity {
 
         taskTitle = (TextView) findViewById(R.id.taskTitle);
 
-        textPriority = (TextView) findViewById(R.id.Taskid);
+        priority = (TextView) findViewById(R.id.taskPriority);
+        priorityColor = (TextView) findViewById(R.id.taskColor);
         description = (TextView) findViewById(R.id.taskDescription);
         dueDate = (TextView) findViewById(R.id.Duedate);
         location = (TextView) findViewById(R.id.location);
@@ -69,12 +56,11 @@ public class TaskDetailInfo extends AppCompatActivity {
         dueDate.setText("Due on: " + DateFormatUtils.getDateFormatted(task.getDateDue()));
         repeatCon.setText("ENTER REPEAT STATUS HERE");
 
+        priority.setText(Integer.toString(task.getPriority()));
+        int color = R.color.colorPriority + task.getPriority();
+        priorityColor.setBackgroundResource(color);
     }
 
-    public void backtotasklist(){
-        Intent intent = new Intent(this, TaskViewList.class);
-        startActivity(intent);
-    }
 
     /**
      * Processes the repeat condition into an readable string for user
