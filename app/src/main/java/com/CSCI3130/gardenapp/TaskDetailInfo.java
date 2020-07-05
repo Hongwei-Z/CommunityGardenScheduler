776b4edbe33092ae5e6bcde95d3e006c5a3d5f20
Logan Sutherland <sutherland@dal.ca>
2020-07-05 12:10:18 -0300

    Improving functionality

--- app/src/main/java/com/CSCI3130/gardenapp/TaskDetailInfo.java
+++ app/src/main/java/com/CSCI3130/gardenapp/TaskDetailInfo.java
@@ -8,17 +8,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.CSCI3130.gardenapp.util.DateFormatUtils;
 import com.CSCI3130.gardenapp.util.data.Task;
 import com.CSCI3130.gardenapp.util.data.User;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
 
 
 public class TaskDetailInfo extends AppCompatActivity {
 
     private Button completeButton;
     private Button backlist;
-    private TextView textName;
-    private TextView textDate;
     private TextView textPriority;
+    private TextView description;
+    private TextView dueDate;
+    private TextView taskTitle;
+    private TextView location;
+    private TextView repeatCon;
+    private Task task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +52,36 @@ public class TaskDetailInfo extends AppCompatActivity {
             }
         });
 
-        textName = findViewById(R.id.submitAuth);
-        textDate = findViewById(R.id.submitDate);
-        textPriority = findViewById(R.id.Taskid);
+        task = (Task) getIntent().getSerializableExtra("t");
+
+        taskTitle = (TextView) findViewById(R.id.taskTitle);
+
+        textPriority = (TextView) findViewById(R.id.Taskid);
+        description = (TextView) findViewById(R.id.taskDescription);
+        dueDate = (TextView) findViewById(R.id.Duedate);
+        location = (TextView) findViewById(R.id.location);
+        repeatCon = (TextView) findViewById(R.id.repeatCondition);
+
+
+        taskTitle.setText(task.getName());
+        description.setText(task.getDescription());
+        location.setText(task.getLocation());
+        dueDate.setText("Due on: " + DateFormatUtils.getDateFormatted(task.getDateDue()));
+        repeatCon.setText("ENTER REPEAT STATUS HERE");
+
     }
 
     public void backtotasklist(){
-        Intent intent = new Intent(this, TaskRegisterDummyPage.class);
+        Intent intent = new Intent(this, TaskViewList.class);
         startActivity(intent);
+    }
+
+    /**
+     * Processes the repeat condition into an readable string for user
+     * @param repeat
+     * @return
+     */
+    public String processRepeatCondition(String repeat) {
+        return "h";
     }
 }
