b4975c609e1737a6219f4f1c20c70c3c7ab9833a
Elizabeth Eddy <el278962@dal.ca>
2020-07-07 21:15:44 -0300

    Refactoring CreateTaskActivity Methods

--- app/src/main/java/com/CSCI3130/gardenapp/create_task/CreateTaskActivity.java
+++ app/src/main/java/com/CSCI3130/gardenapp/create_task/CreateTaskActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -28,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This is the Create task screen where the user can create a task and upload to the database
@@ -36,16 +36,13 @@ import java.util.Objects;
  */
 public class CreateTaskActivity extends AppCompatActivity {
     TaskDatabase db;
-    Spinner weatherSpinner;
-    ArrayAdapter<CharSequence> adapter;
+    private Spinner weatherSpinner, repeatSpinner;
+    ArrayAdapter<CharSequence> repeatAdapter, weatherAdapter;
     private int current_priority;
     private long dueDateSelected = System.currentTimeMillis();
     private boolean edit;
-    private Spinner repeatSpinner;
     private MaterialButtonToggleGroup conditionsToggle;
     private Button dateConditions;
-    private Spinner repeatConditions;
-    private Spinner weatherConditions;
     private DatePickerDialog datePickerDialog;
     private Calendar date;
 
@@ -59,40 +56,36 @@ public class CreateTaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
         edit = getIntent().getBooleanExtra(getString(R.string.editSetting_extra), false);
-        repeatSpinner = (Spinner) findViewById(R.id.repeatSpinner);
-        ArrayAdapter<CharSequence> repeatAdapter = ArrayAdapter.createFromResource(this, R.array.repeat_choice_array, android.R.layout.simple_spinner_dropdown_item);
-        repeatAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        repeatSpinner.setAdapter(repeatAdapter);
         db = new TaskDatabase();
-        //set up weather spinner
-        weatherSpinner = findViewById(R.id.weatherSpinner);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, CurrentWeather.spinnerWeatherConditions);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        weatherSpinner.setAdapter(adapter);
-        Button dueDate = findViewById(R.id.dueDate);
-        dueDate.setText("Due: " + DateFormatUtils.getDateFormatted(System.currentTimeMillis()));
+
         MaterialButton priority1 = findViewById(R.id.buttonPriority1);
         priority1.setBackgroundColor(getColor(R.color.colorPriority1));
         current_priority = 1;
+
+        //set up repeat spinner
+        repeatSpinner = findViewById(R.id.repeatSpinner);
+        repeatAdapter = ArrayAdapter.createFromResource(this, R.array.repeat_choice_array, android.R.layout.simple_spinner_dropdown_item);
+        repeatAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        repeatSpinner.setAdapter(repeatAdapter);
+
+        //set up weather spinner
+        weatherSpinner = findViewById(R.id.weatherSpinner);
+        weatherAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, CurrentWeather.spinnerWeatherConditions);
+        weatherAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        weatherSpinner.setAdapter(weatherAdapter);
+
         conditionsToggle = findViewById(R.id.taskTypesToggleGroup);
         dateConditions = findViewById(R.id.dueDate);
-        repeatConditions = findViewById(R.id.repeatSpinner);
-        weatherConditions = findViewById(R.id.weatherSpinner);
+        dateConditions.setText("Due: " + DateFormatUtils.getDateFormatted(System.currentTimeMillis()));
 
         date = Calendar.getInstance();
-        Button dueDateButton = findViewById(R.id.dueDate);
-        int year = date.get(Calendar.YEAR);
-        int month = date.get(Calendar.MONTH);
-        int day = date.get(Calendar.DAY_OF_MONTH);
-        datePickerDialog = new DatePickerDialog(CreateTaskActivity.this, new DatePickerDialog.OnDateSetListener() {
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                date.set(Calendar.YEAR, year);
-                date.set(Calendar.MONTH, month);
-                date.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                dueDateSelected = date.getTimeInMillis();
-                dueDateButton.setText("Due: " + DateFormatUtils.getDateFormatted(dueDateSelected));
-            }
-        }, year, month, day);
+        datePickerDialog = new DatePickerDialog(CreateTaskActivity.this, (view, year, month, dayOfMonth) -> {
+            date.set(Calendar.YEAR, year);
+            date.set(Calendar.MONTH, month);
+            date.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            dueDateSelected = date.getTimeInMillis();
+            dateConditions.setText("Due: " + DateFormatUtils.getDateFormatted(dueDateSelected));
+        }, date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
 
         if (edit) { // configures the UI to EDIT mode
             loadEditConfiguration((Task) Objects.requireNonNull(getIntent().getSerializableExtra(getString(R.string.task_extra))));
@@ -115,24 +108,12 @@ public class CreateTaskActivity extends AppCompatActivity {
         editLocation.setText(t.getLocation());
         current_priority = t.getPriority();
 
-        boolean repeatedFalse = t.getRepeated().equals("repeat-none");
-        boolean weatherFalse = t.getWeatherTrigger().equals(WeatherCondition.NONE);
-        boolean dateFalse = t.getDateDue() == -1;
-
-        if (repeatedFalse && weatherFalse) {
-            conditionsToggle.check(R.id.dateTypeButton);
-            dateConditions.setVisibility(View.VISIBLE);
-            repeatConditions.setVisibility(View.INVISIBLE);
-            weatherConditions.setVisibility(View.INVISIBLE);
-            Button dueDateButton = findViewById(R.id.dueDate);
-            dueDateButton.setText("Due: " + DateFormatUtils.getDateFormatted(t.getDateDue()));
-            date.setTimeInMillis(t.getDateDue());
-            datePickerDialog.updateDate(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
-        } else if (weatherFalse && dateFalse) {
-            conditionsToggle.check(R.id.repeatTypeButton);
-            dateConditions.setVisibility(View.INVISIBLE);
-            repeatConditions.setVisibility(View.VISIBLE);
-            weatherConditions.setVisibility(View.INVISIBLE);
+        //determine the task condition
+        boolean dateConditionTask = t.getDateDue() != -1;
+        boolean repeatConditionTask = !t.getRepeated().equals("repeat-none");
+        boolean weatherConditionTask = !t.getWeatherTrigger().equals(WeatherCondition.NONE);
+        if (repeatConditionTask) {
+            setToggleVisibility("repeat");
             switch (t.getRepeated()) {
                 case "repeat-weekly":
                     repeatSpinner.setSelection(1);
@@ -144,38 +125,54 @@ public class CreateTaskActivity extends AppCompatActivity {
                     repeatSpinner.setSelection(0);
                     break;
             }
-        } else if (dateFalse && repeatedFalse) {
-            conditionsToggle.check(R.id.weatherTypeButton);
-            dateConditions.setVisibility(View.INVISIBLE);
-            repeatConditions.setVisibility(View.INVISIBLE);
-            weatherConditions.setVisibility(View.VISIBLE);
+        } else if (weatherConditionTask) {
+            setToggleVisibility("weather");
             weatherSpinner.setSelection(Arrays.asList(WeatherCondition.values()).indexOf(t.getWeatherTrigger()));
+        } else if (dateConditionTask) {
+            setToggleVisibility("date");
+            Button dueDateButton = findViewById(R.id.dueDate);
+            dueDateButton.setText("Due: " + DateFormatUtils.getDateFormatted(t.getDateDue()));
+            date.setTimeInMillis(t.getDateDue());
+            datePickerDialog.updateDate(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
         }
     }
 
-
-    public void ontoggleCondition(View view) {
+    /**
+     * Display task condition selectors based on toggled button
+     *
+     * @param view contains the id of what pressed the button
+     */
+    public void onToggleCondition(View view) {
         int id = view.getId();
-        Button dateConditions = findViewById(R.id.dueDate);
-        Spinner repeatConditions = findViewById(R.id.repeatSpinner);
-        Spinner weatherConditions = findViewById(R.id.weatherSpinner);
         switch (id) {
             case R.id.dateTypeButton:
-                //reset repeat and weather
-                dateConditions.setVisibility(View.VISIBLE);
-                repeatConditions.setVisibility(View.INVISIBLE);
-                weatherConditions.setVisibility(View.INVISIBLE);
+                setToggleVisibility("date");
                 break;
             case R.id.repeatTypeButton:
-                dateConditions.setVisibility(View.INVISIBLE);
-                repeatConditions.setVisibility(View.VISIBLE);
-                weatherConditions.setVisibility(View.INVISIBLE);
+                setToggleVisibility("repeat");
                 break;
             case R.id.weatherTypeButton:
-                dateConditions.setVisibility(View.INVISIBLE);
-                repeatConditions.setVisibility(View.INVISIBLE);
-                weatherConditions.setVisibility(View.VISIBLE);
+                setToggleVisibility("weather");
                 break;
+        }
+    }
+
+    private void setToggleVisibility(String condition) {
+        if (condition.equals("date")) {
+            conditionsToggle.check(R.id.dateTypeButton);
+            dateConditions.setVisibility(View.VISIBLE);
+            repeatSpinner.setVisibility(View.INVISIBLE);
+            weatherSpinner.setVisibility(View.INVISIBLE);
+        } else if (condition.equals("repeat")) {
+            conditionsToggle.check(R.id.repeatTypeButton);
+            dateConditions.setVisibility(View.INVISIBLE);
+            repeatSpinner.setVisibility(View.VISIBLE);
+            weatherSpinner.setVisibility(View.INVISIBLE);
+        } else if (condition.equals("weather")) {
+            conditionsToggle.check(R.id.weatherTypeButton);
+            dateConditions.setVisibility(View.INVISIBLE);
+            repeatSpinner.setVisibility(View.INVISIBLE);
+            weatherSpinner.setVisibility(View.VISIBLE);
         }
     }
 
@@ -234,8 +231,6 @@ public class CreateTaskActivity extends AppCompatActivity {
                 priority5.setBackgroundColor(getColor(R.color.colorPriority5));
                 break;
         }
-
-
     }
 
     /**
@@ -250,6 +245,7 @@ public class CreateTaskActivity extends AppCompatActivity {
         String title = editTitle.getText().toString();
         String description = editDescription.getText().toString();
         String location = editLocation.getText().toString();
+        //initialize repeat conditions
         WeatherCondition weatherCondition = WeatherCondition.NONE;
         String repeated = "repeat-none";
         long dateDue = -1;
@@ -275,7 +271,6 @@ public class CreateTaskActivity extends AppCompatActivity {
             case R.id.weatherTypeButton:
                 weatherCondition = WeatherCondition.values()[weatherSpinner.getSelectedItemPosition()];
                 break;
-
         }
 
         ArrayList<CreateTaskError> errors = verifyTask(
@@ -283,7 +278,6 @@ public class CreateTaskActivity extends AppCompatActivity {
                 description,
                 location);
         if (errors.size() == 0) {
-
             // package into a task object
             uploadTask(title, description, current_priority, location, weatherCondition, repeated, dateDue);
             Intent i = new Intent(this, TaskViewList.class);
@@ -326,10 +320,35 @@ public class CreateTaskActivity extends AppCompatActivity {
             task.setWeatherTrigger(weatherTrigger);
             task.setPriority(priority);
             task.setRepeated(repeated);
-            task.setDateDue(dateDue);
+            switch (repeated) {
+                case "repeat-2day":
+                    task.setDateDue(System.currentTimeMillis()+TimeUnit.DAYS.toMillis(2));
+                    break;
+                case "repeat-weekly":
+                    task.setDateDue(System.currentTimeMillis()+TimeUnit.DAYS.toMillis(7));
+                    break;
+                case "repeat-monthly":
+                    task.setDateDue(System.currentTimeMillis()+TimeUnit.DAYS.toMillis(30));
+                    break;
+                default:
+                    task.setDateDue(dateDue);
+            }
             db.updateTask(task);
         } else {
-            Task task = new Task(title, description, priority, "", location, weatherTrigger, dateDue, repeated);
+            Task task;
+            switch (repeated) {
+                case "repeat-2day":
+                    task = new Task(title, description, priority, "", location, weatherTrigger, System.currentTimeMillis()+TimeUnit.DAYS.toMillis(2), repeated);
+                    break;
+                case "repeat-weekly":
+                    task = new Task(title, description, priority, "", location, weatherTrigger, System.currentTimeMillis()+TimeUnit.DAYS.toMillis(7), repeated);
+                    break;
+                case "repeat-monthly":
+                    task = new Task(title, description, priority, "", location, weatherTrigger, System.currentTimeMillis()+TimeUnit.DAYS.toMillis(30), repeated);
+                    break;
+                default:
+                    task = new Task(title, description, priority, "", location, weatherTrigger, dateDue, repeated);
+            }
             db.uploadTask(task);
         }
     }
@@ -358,7 +377,7 @@ public class CreateTaskActivity extends AppCompatActivity {
     /**
      * Used to open the Date Picker Dialog
      *
-     * @param view
+     * @param view contains the id of what pressed the button
      */
     public void openCalendar(View view) {
         datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
