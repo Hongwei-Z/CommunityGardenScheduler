b1fbe2bbdeef2d67323b38339a3341edf6e32069
Arjav Gupta <arjav.tumba@gmail.com>
2020-07-14 18:09:33 -0300

    Merged master into branch

--- app/src/androidTest/java/com/CSCI3130/gardenapp/SignInEspressoTests.java
+++ app/src/androidTest/java/com/CSCI3130/gardenapp/SignInEspressoTests.java
@@ -1,8 +1,6 @@
 package com.CSCI3130.gardenapp;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.uiautomator.UiDevice;
 
 import com.CSCI3130.gardenapp.util.db.DatabaseAuth;
 
@@ -17,9 +15,6 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
