d8846ffbdb90e64d452a155dee2c79f31ee822ef
Elizabeth Eddy <el278962@dal.ca>
2020-07-26 13:48:14 -0300

    Create espresso tests for inactive weather icons and compeleted task icons

--- app/src/androidTest/java/com/CSCI3130/gardenapp/task_view/task_view_list/HighlightEspressoTests.java
+++ app/src/androidTest/java/com/CSCI3130/gardenapp/task_view/task_view_list/HighlightEspressoTests.java
@@ -3,6 +3,8 @@ package com.CSCI3130.gardenapp.task_view.task_view_list;
 import android.content.Intent;
 import android.util.Log;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.CSCI3130.gardenapp.util.data.CurrentWeather;
 import com.CSCI3130.gardenapp.util.data.TaskGenerator;
 import org.hamcrest.core.AllOf;
 import org.junit.After;
@@ -17,12 +19,17 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static com.CSCI3130.gardenapp.task_view.task_view_list.SortListEspressoTests.withRecyclerView;
 import static com.CSCI3130.gardenapp.task_view.task_view_list.DrawableMatcher.withDrawable;
+import static org.mockito.AdditionalMatchers.not;
+
 import androidx.test.rule.ActivityTestRule;
 import com.CSCI3130.gardenapp.R;
 import com.CSCI3130.gardenapp.task_view.ActiveTaskListContext;
 import com.CSCI3130.gardenapp.util.TaskRepeatCondition;
 import com.CSCI3130.gardenapp.util.data.Task;
+import com.CSCI3130.gardenapp.util.data.WeatherCondition;
 import com.CSCI3130.gardenapp.util.db.TaskTestDatabase;
+
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class HighlightEspressoTests {
@@ -83,6 +90,45 @@ public class HighlightEspressoTests {
 
         onView(withRecyclerView(R.id.recycleview_tasks).atPositionOnView(0, R.id.task_due_symbol))
                 .check(matches(withDrawable(R.drawable.yellow)));
+    }
+
+    @Test
+    public void InactiveWeatherIcon() throws InterruptedException {
+        CurrentWeather.currentWeatherList = new ArrayList<>();
+        CurrentWeather.currentWeatherList.add(WeatherCondition.DRY);
+
+        Task taskActive = TaskGenerator.generateTask(false, WeatherCondition.DRY);
+        testDB.uploadTask(taskActive);
+        Task taskInactiveRain = TaskGenerator.generateTask(false, WeatherCondition.RAIN);
+        testDB.uploadTask(taskInactiveRain);
+        Task taskInactiveCold = TaskGenerator.generateTask(false, WeatherCondition.COLD);
+        testDB.uploadTask(taskInactiveCold);
+        Thread.sleep(4000);
+
+        onView(withRecyclerView(R.id.recycleview_tasks).atPositionOnView(0, R.id.task_due_symbol))
+                .check(matches(withDrawable(R.drawable.red)));
+        onView(withRecyclerView(R.id.recycleview_tasks).atPositionOnView(1, R.id.task_due_symbol))
+                .check(matches(withDrawable(R.drawable.inactive)));
+        onView(withRecyclerView(R.id.recycleview_tasks).atPositionOnView(2, R.id.task_due_symbol))
+                .check(matches(withDrawable(R.drawable.inactive)));
+    }
+
+    @Test
+    public void CompletedTaskIcon() throws InterruptedException {
+        long timePast = System.currentTimeMillis() - 1000;
+
+        Task taskComplete = TaskGenerator.generateTask(false, timePast);
+        taskComplete.setDateCompleted(timePast);
+        testDB.uploadTask(taskComplete);
+        Task taskIncomplete = TaskGenerator.generateTask(false, timePast);
+        testDB.uploadTask(taskIncomplete);
+
+        Thread.sleep(4000);
+
+        onView(withRecyclerView(R.id.recycleview_tasks).atPositionOnView(0, R.id.task_due_symbol))
+                .check(matches(withDrawable(R.drawable.completed)));
+        onView(withRecyclerView(R.id.recycleview_tasks).atPositionOnView(1, R.id.task_due_symbol))
+                .check(matches(withDrawable(not(R.drawable.completed))));
     }
 
     @Test
