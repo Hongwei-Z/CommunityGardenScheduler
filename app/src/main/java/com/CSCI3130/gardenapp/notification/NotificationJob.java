6c532a7b6057233cbaf7d77e93ff0101cf13a8cf
Logan Sutherland <sutherland@dal.ca>
2020-07-27 18:51:59 -0300

    Added functionality to send notification to users when they are near an open task, or a task they are assigned to

--- app/src/main/java/com/CSCI3130/gardenapp/notification/NotificationJob.java
+++ app/src/main/java/com/CSCI3130/gardenapp/notification/NotificationJob.java
@@ -1,22 +1,32 @@
 package com.CSCI3130.gardenapp.notification;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import com.CSCI3130.gardenapp.R;
 import com.CSCI3130.gardenapp.db.TaskDatabase;
+import com.CSCI3130.gardenapp.db.DatabaseAuth;
 import com.CSCI3130.gardenapp.task_view.ActiveTaskListContext;
 import com.CSCI3130.gardenapp.task_actions.TaskDetailInfo;
 import com.CSCI3130.gardenapp.util.data.CurrentWeather;
 import com.CSCI3130.gardenapp.util.data.Task;
 import com.CSCI3130.gardenapp.util.data.WeatherCondition;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import androidx.core.app.ActivityCompat;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,6 +41,9 @@ public class NotificationJob extends JobService {
     protected static Map<Integer, Task> sentNotifications = new HashMap<>();
     protected static TaskDatabase db = new TaskDatabase();
     private static int count = 0;
+    private Location currLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
 
     static {
         db.setDbRead(ActiveTaskListContext.MY_TASKS);
@@ -134,6 +147,64 @@ public class NotificationJob extends JobService {
                 .setContentTitle(task.getName() + getString(R.string.weather_title))
                 .setStyle(new NotificationCompat.BigTextStyle()
                         .bigText("It is currently " + task.getWeatherTrigger().name() + "."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+        notificationManager.notify(count, builder.build());
+    }
+
+    /**
+     * Checks if an alert satisfies the Location Alert criteria. Currently set to comparing the user's current location and the location of the task
+     *
+     * @param task Task to verify
+     * @return boolean value if condition is satisfied
+     */
+    protected boolean checkValidLocationAlert(Task task) {
+        if (DatabaseAuth.getCurrentUser().getId().equals(task.getUser()) || task.getUser().equals("")) {//if open or assigned task
+            //check user perms
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return false; //if location perms not given do not send notification
+            }
+
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {//get user's location
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        currLocation = location;
+                    }
+                }
+            });
+
+            //get lat and long values from task object
+            double lat = Double.parseDouble((task.getLocation().split(",")[0]));
+            double lng = Double.parseDouble((task.getLocation().split(" ")[1]));
+
+            //if user's location is the same as the task's
+            if (currLocation.getLongitude() == lng && currLocation.getLatitude() == lat) {
+                return true;//send notification
+            }
+            else {
+                return false;//do not send notification
+            }
+        }
+        else {//if task is assigned to someone else
+            return false;//do not send notification
+        }
+    }
+
+    private void sendLocationAlert(Task task) {
+        count++;
+        notificationManager.cancel(count);
+        sentNotifications.put(count, task);
+
+        Intent intent = new Intent(getApplicationContext(), TaskDetailInfo.class);
+        intent.putExtra(getString(R.string.task_extra), task);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), count, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
+                .setContentTitle(getString(R.string.location_title) + task.getName())
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(task.getDescription()))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent);
